451911367135603f7a033f908615bc00ba491e42
Rishabh Nehra <rishabhnehrapersonal@gmail.com>
2018-07-18 03:10:46 +0530

    Done with Displaying a List

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -1,6 +1,6 @@
 import { Hero } from './Hero';
 
-export const heroes : Hero[] = [
+export const HEROES : Hero[] = [
     { id: 11, name: 'Mr. Nice' },
     { id: 12, name: 'Narco' },
     { id: 13, name: 'Bombasto' },
